795b4c3504b7f448fa9ce7b0c2135eaa85238989
zgg <neveresc@163.com>
2018-08-27 14:23:51 +0800

    项目搭建-logtool

---
+++ toolkit-log/src/test/java/zgg/toolkit/logtool/ToolkitLogApplicationTests.java
@@ -0,0 +1,16 @@
+package zgg.toolkit.logtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToolkitLogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
