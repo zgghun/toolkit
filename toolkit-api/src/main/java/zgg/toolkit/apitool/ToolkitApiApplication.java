72e60766561ec8ebf4f7246485f2e5288fce5d8e
zgg <neveresc@163.com>
2018-08-27 14:12:03 +0800

    项目搭建

---
+++ toolkit-api/src/main/java/zgg/toolkit/apitool/ToolkitApiApplication.java
@@ -0,0 +1,15 @@
+package zgg.toolkit.apitool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zgg
+ */
+@SpringBootApplication
+public class ToolkitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolkitApiApplication.class, args);
+    }
+}
