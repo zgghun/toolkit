b32958d868068a8348d85c3bd9bf0b9a082746c5
zgg <neveresc@163.com>
2018-09-03 18:05:02 +0800

    换成gson

--- toolkit-core/src/main/java/zgg/toolkit/core/config/WebMvcConfig.java
+++ toolkit-core/src/main/java/zgg/toolkit/core/config/WebMvcConfig.java
@@ -1,10 +1,7 @@
 package zgg.toolkit.core.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * Created by zgg on 2018/08/31
@@ -15,29 +12,6 @@ import java.util.List;
  */
 
 @Configuration
-//@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-//
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        MappingJackson2HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter();
-//        JavaTimeModule javaTimeModule = new JavaTimeModule();
-//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-//        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd********")));
-//        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-//
-//
-////         大数序列化后，Long转json精度丢失的问题
-//        SimpleModule simpleModule = new SimpleModule();
-//        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-//        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-//        simpleModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
-//
-//        ObjectMapper objectMapper = jacksonConverter.getObjectMapper();
-//        objectMapper.registerModule(javaTimeModule);
-//        jacksonConverter.setObjectMapper(objectMapper);
-//
-//        converters.add(jacksonConverter);
-    }
 
 }
