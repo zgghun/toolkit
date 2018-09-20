70997c63462cc9fd4cf876160fefca8f86014531
zgg <neveresc@163.com>
2018-09-20 17:29:52 +0800

    一些修改

--- toolkit-core/src/test/java/zgg/toolkit/core/ToolkitCoreApplicationTests.java
+++ toolkit-core/src/test/java/zgg/toolkit/core/ToolkitCoreApplicationTests.java
@@ -1,16 +1,26 @@
 package zgg.toolkit.core;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import zgg.toolkit.core.page.PageParam;
+import zgg.toolkit.core.utils.IdWorker;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ToolkitCoreApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void test1(){
+        PageParam pageParam = new PageParam();
+        pageParam.setPageIndex(-1);
+        pageParam.setPageSize(null);
+        System.out.println(pageParam.getPageIndex() + "---" + pageParam.getPageSize());
+    }
+
+    @Test
+    public void contextLoads() {
+        for (int i = 0; i < 1; i++) {
+            System.out.println(IdWorker.getNextId());
+        }
+    }
 
 }
